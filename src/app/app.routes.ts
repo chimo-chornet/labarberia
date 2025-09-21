8e5be679a26b9f07be9093fcef56be691fd77b3b
Chimo <chimochornet@gmail.com>
2025-09-21 20:50:28 +0200

    pasado todo a MongoDb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { AreaUsuarioComponent } from './components/area-usuario/area-usuario.component';
+import { HomeComponent } from './components/home/home.component';
+import { Login } from './components/login/login';
+import { CalUsuarioComponent } from './components/cal-usuario/cal-usuario.component';
+import { ModpassComponent } from './components/modpass/modpass.component';
+import { CitasComponent } from './components/citas/citas.component';
+import { CitaUsuarioComponent } from './components/cita-usuario/cita-usuario.component';
+import { CalComponent } from './components/cal/cal.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { AreaAdminComponent } from './components/area-admin/area-admin.component';
+import { CitaDia } from './cita-dia/cita-dia';
+import { Pruebas } from './components/pruebas/pruebas';
+import { CitaAdmin } from './components/cita-admin/cita-admin';
+import { CitasAdminComponent } from './components/citas-admin/citas-admin';
+import { DialogOverviewExample } from './components/emergente/emergente';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path: 'usuarios',component:AreaUsuarioComponent},
+    {path: 'home',component:HomeComponent},
+    {path: 'login',component:Login},
+    {path: 'calusuario',component:CalUsuarioComponent},
+    {path: 'modpass',component:ModpassComponent},
+    {path: 'citas',component:CitasComponent},
+    {path: 'citaUsuario',component:CitaUsuarioComponent},
+    {path: 'citaAdmin',component:CitaAdmin},
+    {path: 'citasAdmin',component:CitasAdminComponent},
+
+
+{path: 'emergente',component:DialogOverviewExample},
+    {path: 'cal',component:CalComponent},
+    {path: 'usuario',component:UsuarioComponent},
+    {path: 'admin',component:AreaAdminComponent},
+    {path: 'citas/dia',component:CitaDia},
+    {path: 'pruebas',component:Pruebas}
+
+
+
+];
--- src/app/components/cita-usuario/cita-usuario.component.ts
+++ src/app/components/cita-usuario/cita-usuario.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 import { Peticiones } from '../../services/peticiones';
-import { Request,Response } from 'express';
-
-import { DataService } from '../../services/data-service.service';
 
 
 @Component({
@@ -19,70 +16,92 @@ export class CitaUsuarioComponent implements OnInit {
   urlTree: UrlTree;
   respuesta: any = [];
   idUsuario:any='';
-  token:string='';
-
+  token:any='';
+  mensaje:string='Mensaje de prueba ';
   frase: any = '';
-  constructor(private router: Router, private conn: Peticiones, private chimo:DataService) {
+  constructor(private router: Router, private conn: Peticiones) {
 
     this.urlTree = this.router.parseUrl(this.router.url);
     this.year = this.urlTree.queryParams['year'];
     this.month = this.urlTree.queryParams['month'];
     this.day = this.urlTree.queryParams['day'];
-    this.idUsuario = this.urlTree.queryParams['user'];
-    this.token = this.urlTree.queryParams['token'];
+    this.idUsuario = localStorage.getItem('userId');
+    if(localStorage.getItem('token')!=null){
+      this.token=localStorage.getItem('token');
+  };
 
 
 
   }
-
+//Al inicio recogemos las horas reservadas para marcarlas en rojo
   ngOnInit(): void {
 
-    console.log(this.year + '-' + this.month + '-' + this.day);
 
-    this.conn.getCitas(this.year+"-"+this.month+"-"+this.day,this.token).subscribe(
+    this.conn.getCitas(this.year+"-"+this.month+"-"+this.day).subscribe(
       (res: any) => {
         this.respuesta = res.body;
 
-        //this.router.navigate(['/usuarios']);
-this.respuesta.forEach((element:any) => {
+      this.respuesta.forEach((element:any) => {
       let cambio=document.getElementById(element.hora);
       if(cambio){
       cambio.style.color='red'
       }
     });
       });
-
-
-
-
   }
 
+  //Al salir redirigimos al area de usuario
   salir() {
-    history.back()
-    history.back()
-
-    //this.router.navigate(['/usuarios']);
+    this.router.navigate(['/usuarios']);
   }
 
 
+//Se selecciona la hora de la cita, se marca en rojo y se añade en la base de datos
 
-  mostrarCitas() {
-    }
   asignaHora(hora:string){
+    this.mensaje='Ha elegido el día: '+ this.day+' a las: '+hora;
     let cambio=document.getElementById(hora);
     if(cambio&&cambio.style.color!='red'){
+      this.modal();
+      let resultado=document.getElementById('ok');
+      if(resultado!=null){
+      resultado.onclick=()=>{
+        cambio.style.color='red';
 
-      cambio.style.color='red';
-
-      this.conn.creaCita(this.day,this.month,this.year,hora,this.idUsuario).subscribe((resp:any)=>{
+        this.conn.creaCita(this.day,this.month,this.year,hora,this.idUsuario).subscribe((resp:any)=>{
 
         this.respuesta=resp.body;
       });
-
-
+      this.cerrarModal();
+      this.salir();
+      this.conn.getCitasUsuario(this.idUsuario).subscribe((res:any)=>{
+    this.respuesta=res.body;
+    if(this.respuesta.length>0){
+      this.mensaje='Citas programadas:'
+    }
+});
+    }
+  }
     }else{
       alert('Hora reservada')
     }
 
   }
+
+
+//Hacemos visible la ventana modal
+modal() {
+  let modal:any = document.getElementById("myModal");
+
+  modal.style.display="block";
+
+}
+
+// Se cierra la ventana modal
+cerrarModal() {let modal:any = document.getElementById("myModal");
+
+  modal.style.display="none";
+}
+
+
 }
